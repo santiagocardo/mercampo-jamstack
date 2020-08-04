1251fcc721634d5c112284774731615b65d803aa
Santiago Cardona <santiagocardo80@gmail.com>
2020-08-04 13:05:09 -0500

    🖼  Change app mockup

--- src/components/app-mockup.js
+++ src/components/app-mockup.js
@@ -5,7 +5,7 @@ import AppImg from '../images/app-mockup.png'
 const AppMockup = () => {
   return (
     <div className="bg-gray-800">
-      <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+      <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 flex flex-col items-center text-center lg:text-left lg:flex-row lg:justify-between">
         <h1 className="text-4xl leading-7 font-extrabold tracking-tight text-gray-100 sm:text-5xl sm:leading-10 md:mx-12">
           Personas a tu alrededor están ordenando frescos productos usando
             <span className="text-green-400"> Mercampo App.</span>
@@ -15,7 +15,7 @@ const AppMockup = () => {
             src={AppImg}
             loading="lazy"
             alt="App Muckup"
-            className="h-full w-full object-cover"
+            className="h-auto w-auto object-cover"
           />
         </div>
       </div>
